f5183611a1268bf42dceada389deffcc444ac4da
cizal <cizal@esignature.com.np>
2024-03-30 20:43:23 +0545

    Generate qr code with id of the business

--- server/controllers/formDataController.js
+++ server/controllers/formDataController.js
@@ -1,10 +1,15 @@
 const FormData = require("../models/formDataModel");
+const qr = require("qrcode");
 
 exports.storeFormData = async (req, res) => {
   try {
     const formData = new FormData(req.body);
-    await formData.save();
-    res.json({ message: "Form data stored successfully" });
+    const savedFormData = await formData.save();
+
+    // Generate QR code with the ObjectId
+    const qrCodeData = await qr.toDataURL(savedFormData._id.toString());
+
+    res.status(200).json(qrCodeData);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
